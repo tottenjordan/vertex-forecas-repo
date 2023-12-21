a097a0c5635c9a335761c05b6f2fcfa23f93f427
tottenjordan <jordantotten@google.com>
2023-12-21 17:15:56 +0000

    tab workflows v1

--- 05-tabular-workflows/README.md
+++ 05-tabular-workflows/README.md
@@ -1,5 +1,7 @@
 # Forecasting with Vertex Tabular Workflows 
 
+> TODO
+
 
 ### L2L pipeline
 <img src='imgs/l2l-full-pipe-run.png'>
---
+++ 05-tabular-workflows/src/create_bq_dataset.py
@@ -0,0 +1,38 @@
+
+import kfp
+from typing import NamedTuple
+from kfp.dsl import (
+    # Artifact, 
+    # Dataset, 
+    # Input, InputPath, 
+    # Model, Output, OutputPath, 
+    component, 
+    Metrics
+)
+
+@component(
+  base_image='python:3.9',
+  packages_to_install=['google-cloud-bigquery==3.14.1'],
+)
+def create_bq_dataset(
+    project: str,
+    # vertex_dataset: str,
+    new_bq_dataset: str,
+    bq_location: str
+) -> NamedTuple('Outputs', [
+    ('bq_dataset_name', str),
+    ('bq_dataset_uri', str),
+]):
+    
+    from google.cloud import bigquery
+
+    bq_client = bigquery.Client(project=project, location='US') # bq_location)
+    (
+      bq_client.query(f'CREATE SCHEMA IF NOT EXISTS `{project}.{new_bq_dataset}`')
+      .result()
+    )
+    
+    return (
+        f'{new_bq_dataset}',
+        f'bq://{project}:{new_bq_dataset}',
+    )
